58f49c7a9382fb5d9d4e977aa5aff03d9a527300
Alexandre Dergham <adergham@xebia.fr>
2013-09-04 12:14:05 +0200

    selection des regions

--- webapp/js/offreController.js
+++ webapp/js/offreController.js
@@ -5,6 +5,26 @@ function OffreCtrl($scope, Offre) {
 
 
     $scope.search = function(){
-        $scope.offres = Offre.query({q:$scope.searchTerm});
+        criteria = {q:"", r:""};
+        if($scope.searchTerm){
+            criteria.q = $scope.searchTerm;
+        }
+        if($scope.selectedRegion){
+            criteria.r = $scope.selectedRegion.id;
+        }
+
+        $scope.offres = Offre.query(criteria);
     };
+
+    $scope.regions = [{ id: "Ile-de-France" },
+        { id: "Régions voisines" },
+        { id: "Toute la France" },
+        { id: "Paris" },
+        { id: "Seine-et-Marne" },
+        { id: "Yvelines" },
+        { id: "Essonne" },
+        { id: "Hauts-de-Seine" },
+        { id: "Seine-Saint-Denis" },
+        { id: "Val-de-Marne" },
+        { id: "Val-d'Oise" } ];
 }

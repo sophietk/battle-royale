33a5290584c99c57bec11cf17f80d1d52362ad27
sophietk <sophie.tk@gmail.com>
2013-09-04 10:54:30 +0200

    Heroku port config

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 global._ = _ = require('underscore');
 
 var express = require('express'),
-    app = express();
-
+    app = express(),
+    port = process.env.PORT || 3000;
 
 app.use(express.bodyParser());
 app.use(express.cookieParser());
 app.use(express.static(__dirname + '/../webapp'));
 app.use(express.favicon());
 
-app.listen(3000);
+app.listen(port);
 console.log("Server is listening for HTTP requests on port 3000...");
 
 app.get('/offres', function(req, res){

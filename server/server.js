555f8b1df2ce32368d492b22f891d20d78ad0c53
sophietk <sophie.tk@gmail.com>
2013-09-04 12:14:33 +0200

    Search by areas

--- server/server.js
+++ server/server.js
@@ -18,15 +18,17 @@ console.log("Server is listening for HTTP requests on port 3000...");
 var options = {
     host: 'www.leboncoin.fr',
     port: 80,
-    path_root: '/annonces/offres/ile_de_france/?f=a&th=1&q='
+    path_root: '/annonces/offres/{city}/?f=a&th=1&q='
 };
-
 
 app.get('/offres', function (req, res) {
 
     var query = req.query['q'];
+    var city = req.query['r'];
+    city = 'ile_de_france' + (req.query['r'] ? '/' + alnumuscorify(city) : '');
 
 	console.log("query", query);
+	console.log("city", city);
 
     var callback = function(response) {
         var str = '';
@@ -46,7 +48,7 @@ app.get('/offres', function (req, res) {
         });
     };
 
-    options.path = options.path_root + query;
+    options.path = (options.path_root + query).replace('{city}', city);
     console.log("options.path", options.path);
 
     http.request(options, callback).end();
@@ -60,7 +62,7 @@ var parse = function(str) {
 
     $(str).find('.list-lbc a').
         each(function(i, elem) {
-            var $elem = $(elem)
+            var $elem = $(elem);
             rez.push({
                 image: $elem.find('.image img').attr('src'),
                 description: $elem.find('.title').text().trim(),
@@ -71,3 +73,5 @@ var parse = function(str) {
 
     return rez;
 };
+
+function alnumuscorify(a){a=a.toLowerCase();a=a.replace(/[áàâä]/g,"a");a=a.replace(/[éèêë]/g,"e");a=a.replace(/[íìîï]/g,"i");a=a.replace(/[óòôö]/g,"o");a=a.replace(/[úùûü]/g,"u");a=a.replace(/ÿ/g,"y");a=a.replace(/ç/g,"c");a=a.replace(/¤/g,"e");a=a.replace(/²/g,"2");a=a.replace(/³/g,"3");a=a.replace(/[^a-zA-Z0-9]+/g,"_");return a}

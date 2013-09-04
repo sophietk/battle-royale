184d4bc4dc9c580dc1e603a2beab720e9ebf1ff2
dimapod <dimapod@gmail.com>
2013-09-04 10:46:10 +0200

    Add server

--- server/server.js
+++ server/server.js
@@ -8,10 +8,9 @@ app.use(express.bodyParser());
 app.use(express.cookieParser());
 app.use(express.static(__dirname + '/../webapp'));
 app.use(express.favicon());
-app.use(logger.express(express));
 
 app.listen(3000);
-logger.info("Server is listening for HTTP requests on port 3000...");
+console.log("Server is listening for HTTP requests on port 3000...");
 
 app.get('/offres', function(req, res){
     res.send({ok: "ok"}, 200);

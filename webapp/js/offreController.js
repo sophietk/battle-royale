60661f2c6cce51014d6ebe2ca4bdbebf8468130e
Alexandre Dergham <adergham@xebia.fr>
2013-09-04 11:53:08 +0200

    debouchonnage de la liste initiale

--- webapp/js/offreController.js
+++ webapp/js/offreController.js
@@ -1,9 +1,7 @@
 function OffreCtrl($scope, Offre) {
 
 
-    //$scope.offres = Offre.query();
-
-    $scope.offres = [{image:"", description:"desc", price:"10"}, {image:"", description:"desc2", price:"15"} ];
+    $scope.offres = Offre.query({q:""});
 
 
     $scope.search = function(){
